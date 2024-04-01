1d4250005e037d5ecead461b9ce1488adc8fb971
AdeebaFarhan <161740584+AdeebaFarhan@users.noreply.github.com>
2024-04-01 13:06:43 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+async function main() {
+  let todo = [];
+  let condition = true;
+
+  while (condition) {
+    const action = await inquirer.prompt([
+      {
+        name: "action",
+        type: "list",
+        message: "Which action do you want to perform?",
+        choices: ["add", "edit", "delete", "exit"],
+      },
+    ]);
+
+    if (action.action === "add") {
+      const addTask = await inquirer.prompt([
+        {
+          name: "todo",
+          type: "input",
+          message: "What task do you want to add to your To-Do list?",
+        },
+        {
+          name: "addMore",
+          type: "confirm",
+          message: "Do you want to continue adding more tasks?",
+          default: false,
+        },
+      ]);
+      todo.push(addTask.todo);
+      condition = addTask.addMore;
+    } else if (action.action === "edit") {
+      const { task } = await inquirer.prompt([
+        {
+          name: "task",
+          type: "list",
+          message: "Select the task you want to edit:",
+          choices: todo,
+        },
+      ]);
+      //const index = todo.indexOf(task); // Find the index of the selected task
+      const { newTask } = await inquirer.prompt([
+        {
+          name: "newTask",
+          type: "input",
+          message: "Enter the new task:",
+        },
+      ]);
+      //todo[index] = newTask; // Update the task at the found index
+    } else if (action.action === "delete") {
+      const { task } = await inquirer.prompt([
+        {
+          name: "task",
+          type: "list",
+          message: "Select the task you want to delete:",
+          choices: todo,
+        },
+      ]);
+      const index = todo.indexOf(task); // Find the index of the selected task
+      todo.splice(index, 1); // Remove the task at the found index
+    } else if (action.action === "exit") {
+      condition = false; // Exit the loop and end the program
+    }
+    console.log("Current To-Do List:");
+    console.log(todo.join("\n")); 
+  }
+
+  console.log("Goodbye!"); // Message displayed upon exiting the loop
+}
+
+main();
